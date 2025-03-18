5e8680ec5d535721cf9304efec06057ba5375050
Ratnakar <ratnakarravada11@gmail.com>
2025-03-18 20:55:25 +0530

    re-deploy

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -8,8 +8,6 @@ export const fetchAuthSession = async () => {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "*",
         "Access-Control-Allow-Credentials": "true",
       },
     }
