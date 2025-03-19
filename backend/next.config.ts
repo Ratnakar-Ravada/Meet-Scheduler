99328ddb4bf5871681ed70b98997a7ffb9720677
Ratnakar <ratnakarravada11@gmail.com>
2025-03-19 22:54:25 +0530

    try another way

--- backend/next.config.ts
+++ backend/next.config.ts
@@ -1,25 +1,31 @@
+const headers = [
+  "Accept",
+  "Accept-Version",
+  "Content-Length",
+  "Content-MD5",
+  "Content-Type",
+  "Date",
+  "X-Api-Version",
+  "X-CSRF-Token",
+  "X-Requested-With",
+];
+
 module.exports = {
+  reactStrictMode: true,
   async headers() {
     return [
       {
-        source: "/api/:path*", // Covers both `/api/auth/:path*` and `/api/meet`
+        source: "/api/(.*)",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.FRONTEND_URL || "*",
-          },
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
           },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: headers.join(", ") },
         ],
       },
     ];
   },
-  output: "standalone",
 };
